2cd77f1e163e73e03a7b021a31617c5b33f7af14
Amin Mirhashemi <amin.mirhashemi@basalam.com>
2023-04-01 19:44:26 +0330

    fix: deploy problems

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();

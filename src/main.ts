2f9bfb2a38924026780a3dfe9ee6b3cbe5f5ff8c
Amin Mirhashemi <amin.mirhashemi@basalam.com>
2023-04-01 19:49:40 +0330

    chore: log env variable

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import * as dotenv from 'dotenv';
 dotenv.config();
-
+console.log(process.env.DATABASE_CONNECTION);
 import { AppModule } from './app.module';
 
 async function bootstrap() {

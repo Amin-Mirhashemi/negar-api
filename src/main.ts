5bd9cbd8f0f9e54e2115f313638d4e6c2700b461
Amin Mirhashemi <amin.mirhashemi@basalam.com>
2023-04-02 03:26:44 +0330

    feat: implement documentation

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,28 @@
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
 import * as dotenv from 'dotenv';
 dotenv.config();
-console.log(process.env.DATABASE_CONNECTION);
+
 import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Negar')
+    .setDescription('Backend Api Documentation')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
